d0b969fcfafdb2d2d17f774c2877eab541ef1deb
HASURO <rentaro170928@icloud.com>
2023-09-06 19:04:41 +0900

    add: focus image

--- src/components/album/AlbumImage.tsx
+++ src/components/album/AlbumImage.tsx
@@ -3,16 +3,33 @@
 import Image from 'next/image';
 import styles from './album.module.scss';
 import {motion} from 'framer-motion';
+import { useState } from 'react';
+import { RxCross2 } from 'react-icons/rx';
 
 export default function AlbumImage({image, alt, number}: {image: any, alt: string, number: number}) {
+    const [focus, setFocus] = useState(false);
+
+    const handleFocus = () => {
+        setFocus(!focus);
+    };
+
     return (
         <motion.div
             className={styles['image-box']}
+            onClick={handleFocus}
             initial={{opacity: 0, y: 6}}
             animate={{opacity: 1, y: 0}}
             transition={{duration: 0.3, delay: number * 0.1}}
         >
             <Image src={image} alt={alt} width={150} className={styles.image} />
+        {focus && (
+            <div className={styles['image-focus-background']} onClick={handleFocus}>
+                <div className={styles['image-focus-background-cushion']}>
+                    <div className={styles['cross-button']}><RxCross2 /></div>
+                    <Image src={image} alt={alt} width={400} className={styles['focus-image']} />
+                </div>
+            </div>
+        )}
         </motion.div>
     )
 }
