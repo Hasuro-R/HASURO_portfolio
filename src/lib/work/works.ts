175c1a8c6bfa355fb67d9bdabc5b5a9413b3e4bf
HASURO <rentaro170928@icloud.com>
2023-09-06 14:13:01 +0900

    add: portfolio work

--- src/lib/work/works.ts
+++ src/lib/work/works.ts
@@ -1,7 +1,9 @@
 import anisphereImage from '@/public/work/AniSphereOGP.png';
 import anisphereAboutImage from '@/public/work/about-open-graph.png';
+import portfolioImage from '@/public/work/HASURO_ogp_image.png';
 
 export const works = [
     {title: 'AniSphere', image: anisphereImage, url: 'https://anisphere.jp', description: 'アニメ共有サイト', techs: ['next', 'rails']},
     {title: 'AniSphere About', image: anisphereAboutImage, url: 'https://about.anisphere.jp', description: 'アニメ共有サイトのAboutページ', techs: ['next']},
+    {title: 'portfolio', image: portfolioImage, url: '', description: 'このサイト', techs: ['next']},
 ];
