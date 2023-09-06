cb2c9c6f81f5b73bef16f4786b009997aa333a26
HASURO <rentaro170928@icloud.com>
2023-09-06 18:35:13 +0900

    fix: header change component

---
+++ src/components/header/Header.tsx
@@ -0,0 +1,19 @@
+import AccountBoxes from '../account/AccountBoxes';
+import MainIcon from '../account/MainIcon';
+import styles from './header.module.scss';
+
+export default function Header({title}: {title: string}) {
+    return (
+        <div className={styles['top-account-container']}>
+            <div className={styles['top-account-container-left']}>
+                <div className={styles['top-account-icon-box']}>
+                <MainIcon />
+                </div>
+                <p className={styles['top-account-name']}>{title}</p>
+            </div>
+            <div className={styles['top-account-container-right']}>
+                <AccountBoxes />
+            </div>
+        </div>
+    );
+}
