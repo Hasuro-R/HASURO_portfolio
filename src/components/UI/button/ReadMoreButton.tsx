bb2519df9f89872aca6d750fee966c9864ddaa85
HASURO <rentaro170928@icloud.com>
2023-09-06 14:12:43 +0900

    add: read more animation

--- src/components/UI/button/ReadMoreButton.tsx
+++ src/components/UI/button/ReadMoreButton.tsx
@@ -1,14 +1,37 @@
 'use client'
 
+import { useState } from 'react';
 import styles from './button.module.scss';
 import {FiArrowRightCircle} from 'react-icons/fi';
+import {motion} from 'framer-motion';
 
 export default function ReadMoreButton() {
+    const [isHover, setIsHover] = useState(false);
+
+    const handleIsHover = () => {
+        setIsHover(true);
+    };
+
+    const handleUnIsHover = () => {
+        setIsHover(false);
+    };
+
     return (
         <div className={styles['more-button']}>
-            <div className={styles['more-button-top']}>
-                More <FiArrowRightCircle />
-            </div>
+            <motion.div
+                className={styles['more-button-top']}
+                onMouseEnter={handleIsHover}
+                onMouseLeave={handleUnIsHover}
+            >
+                More
+                <motion.div
+                    className={styles['more-button-arrow']}
+                    animate={isHover ? {x: 5} : {x: 0}}
+                    transition={{duration: 0.4}}
+                >
+                    <FiArrowRightCircle />
+                </motion.div>
+            </motion.div>
         </div>
     );
 }
