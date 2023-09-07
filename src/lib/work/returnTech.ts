010c041dab4d53cfe04b2f14973bdfa28a498867
HASURO <rentaro170928@icloud.com>
2023-09-07 19:03:38 +0900

    add: new tech

--- src/lib/work/returnTech.ts
+++ src/lib/work/returnTech.ts
@@ -3,7 +3,11 @@ export const returnTech = (tech: string) => {
         case "rails":
             return {text: 'Rails', color: 'rgb(235, 100, 92)'};
         case 'next':
-            return {text: 'Next.js', color: 'rgb(109, 152, 208)'};
+            return {text: 'Next.js', color: 'rgb(0, 0, 0)'};
+        case 'ts':
+            return {text: 'TypeScript', color: 'rgb(109, 152, 208)'};
+        case 'sass':
+            return {text: 'Sass', color: 'rgb(234, 94, 127)'};
         default:
             return {text: 'unknown', color: 'rgb(120, 140, 146)'}
     };
