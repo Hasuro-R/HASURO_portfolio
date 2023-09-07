d4483a4b0524510ff47876e59cb03454bbcd846c
HASURO <rentaro170928@icloud.com>
2023-09-07 13:00:15 +0900

    add: fish album

---
+++ src/lib/image/fishImages.ts
@@ -0,0 +1,45 @@
+import aohata from '@/public/album/fish/aohata.png';
+import buriSashimi from '@/public/album/fish/buri_sashimi.jpg';
+import burisyabu from '@/public/album/fish/burisyabu.jpg';
+import carpaccio from '@/public/album/fish/carpaccio.jpg';
+import chouzameSashimi from '@/public/album/fish/chouzame_sashimi.jpg';
+import chouzameSushi from '@/public/album/fish/chouzame_sushi.jpg';
+import chouzameAll from '@/public/album/fish/chouzame_all.png';
+import chouzame from '@/public/album/fish/chouzame.png';
+import itoyoriSushi from '@/public/album/fish/itoyori_sushi.jpg';
+import kasago from '@/public/album/fish/kasago.png';
+import kawahagiSashimi from '@/public/album/fish/kawahagi_sashimi.png';
+import kinkiSashimi from '@/public/album/fish/kinki_sashimi.jpg';
+import kinki from '@/public/album/fish/kinki.png';
+import nannyouakamutu from '@/public/album/fish/nannyouakamutu.png';
+import oomonhata from '@/public/album/fish/oomonhata.png';
+import sawaraDressing from '@/public/album/fish/sawara_dressing.jpg';
+import simesaba from '@/public/album/fish/simesaba.jpg';
+import siromiSushi from '@/public/album/fish/siromi_sushi.jpg';
+import sashimori from '@/public/album/fish/sashimori.jpg';
+import shiromiSashimi from '@/public/album/fish/shiromi_sashimi.jpg';
+
+export const fishImages = [
+    {image: sashimori, alt: 'sashimi'},
+    {image: carpaccio, alt: 'carpaccio'},
+    {image: chouzame, alt: 'chouzame'},
+    {image: nannyouakamutu, alt: 'nannyouakamutu'},
+    {image: simesaba, alt: 'simesaba'},
+    {image: buriSashimi, alt: 'buri sashimi'},
+    {image: aohata, alt: 'aohata'},
+    {image: chouzameAll, alt: 'chouzame'},
+    {image: chouzameSashimi, alt: 'chouzame sashimi'},
+    {image: chouzameSushi, alt: 'chouzame sushi'},
+    {image: burisyabu, alt: 'burisyabu'},
+    {image: sawaraDressing, alt: 'sawara'},
+    {image: kasago, alt: 'kasago'},
+    {image: kinki, alt: 'kinki'},
+    {image: kinkiSashimi, alt: 'kinki sashimi'},
+    {image: itoyoriSushi, alt: 'itoyori sushi'},
+    {image: shiromiSashimi, alt: 'sashimi'},
+    {image: oomonhata, alt: 'oomonhata'},
+    {image: kawahagiSashimi, alt: 'kawahagi sashimi'},
+    {image: siromiSushi, alt: 'sushi'},
+];
+
+export const firstFishImages = fishImages.slice(0, 4);
