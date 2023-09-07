3015d77699c23fbc85202544c34802ec16b42585
HASURO <rentaro170928@icloud.com>
2023-09-07 19:03:58 +0900

    add: timeline

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import styles from './page.module.scss'
 import AboutMe from '@/components/home/AboutMe'
 import Works from '@/components/home/Works'
 import Header from '@/components/header/Header'
+import Timeline from '@/components/home/Timeline'
 
 export default function Home() {
   return (
@@ -9,6 +10,7 @@ export default function Home() {
     <Header title='HASURO' />
     <AboutMe />
     <Works />
+    <Timeline />
     </>
   )
 }
