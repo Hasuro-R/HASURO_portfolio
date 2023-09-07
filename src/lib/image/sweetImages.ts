59ec2beaddbaa4d134d622ae501e160c41289579
HASURO <rentaro170928@icloud.com>
2023-09-07 12:59:36 +0900

    add: sweet album

---
+++ src/lib/image/sweetImages.ts
@@ -0,0 +1,31 @@
+import appleTart from '@/public/album/sweet/apple_tart.png';
+import bakedChocolateCake from '@/public/album/sweet/baked_chocolate_cake.jpg';
+import cannere from '@/public/album/sweet/cannere.png';
+import carafulCannere from '@/public/album/sweet/caraful_cannere.jpg';
+import carafulCannereSideway from '@/public/album/sweet/caraful_cannere_sideway.jpg';
+import chocolateCake from '@/public/album/sweet/chocorate_cake.png';
+import firstConan from '@/public/album/sweet/first_conan.jpg';
+import lemonCake from '@/public/album/sweet/lemon_cake.png';
+import opera from '@/public/album/sweet/opera.jpg';
+import sachertorte from '@/public/album/sweet/sachertorte.jpg';
+import tarttatan from '@/public/album/sweet/tarttatan.jpg';
+import snowman from '@/public/album/sweet/snowman.jpg';
+import wornatCake from '@/public/album/sweet/wornat_cake.png';
+
+export const sweetImages = [
+    {image: carafulCannere, alt: 'caraful cannere'},
+    {image: chocolateCake, alt: 'chocolate cake'},
+    {image: lemonCake, alt: 'lemon cake'},
+    {image: firstConan, alt: 'cheese cake'},
+    {image: wornatCake, alt: 'walnut cake'},
+    {image: opera, alt: 'opera'},
+    {image: cannere, alt: 'cannere'},
+    {image: snowman, alt: 'snowman'},
+    {image: sachertorte, alt: 'sachertorte'},
+    {image: tarttatan, alt: 'tarttatan'},
+    {image: carafulCannereSideway, alt: 'caraful cannere'},
+    {image: appleTart, alt: 'apple tart'},
+    {image: bakedChocolateCake, alt: 'baked chocolate cake'},
+];
+
+export const firstSweetImages = sweetImages.slice(0, 4);
