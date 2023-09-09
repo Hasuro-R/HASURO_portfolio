e5eb7d0de0785df21df086ba5203a590472472f1
HASURO <rentaro170928@icloud.com>
2023-09-10 01:08:12 +0900

    add: canada album

--- src/lib/image/canadaImages.ts
+++ src/lib/image/canadaImages.ts
@@ -10,13 +10,29 @@ import graffitiEye from '@/public/album/canada/graffiti_eye.png';
 import bridge from '@/public/album/canada/bridge.png';
 import unionStation from '@/public/album/canada/union_station.png';
 import eveningUnion from '@/public/album/canada/evening_union.png';
-import mySilhouette from '@/public/album/canada/my_silhouette.png'
+import mySilhouette from '@/public/album/canada/my_silhouette.png';
+import cafe from '@/public/album/canada/10deans.jpeg';
+import art from '@/public/album/canada/art.jpeg';
+import cakes from '@/public/album/canada/cakes.jpeg';
+import canadaFall from '@/public/album/canada/canada_fall.jpeg';
+import chocolateBuilding from '@/public/album/canada/chocolate_building.jpeg';
+import cliffAndTree from '@/public/album/canada/cliff_tree.jpeg';
+import library from '@/public/album/canada/library.png';
+import market from '@/public/album/canada/market.jpeg';
+import niagaraBehind from '@/public/album/canada/niagara_behind.png';
+import nightBear from '@/public/album/canada/night_amusument_park.jpg';
+import oldBuilding from '@/public/album/canada/old_building.jpeg';
+import rappar from '@/public/album/canada/rappar.jpg';
+import squirrel from '@/public/album/canada/squirrel.jpeg';
+import torontoUnivercity from '@/public/album/canada/toronto_univercity.jpeg';
+import turtle from '@/public/album/canada/turtle.jpeg';
 
 export const canadaImages = [
     {image: nightUnionNear, alt: 'night union view'},
     {image: niagara, alt: 'niagara'},
     {image: graffiti, alt: 'graffiti'},
     {image: boat, alt: 'boat'},
+    {image: art, alt: 'art'},
     {image: nightUnionFar, alt: 'night union view'},
     {image: casaLoma, alt: 'Casa Loma'},
     {image: mySilhouette, alt: 'my silhouette'},
@@ -26,6 +42,20 @@ export const canadaImages = [
     {image: bridge, alt: 'bridge'},
     {image: unionStation, alt: 'union station view'},
     {image: eveningUnion, alt: 'evening union view'},
+    {image: cakes, alt: 'supermarket cakes'},
+    {image: cafe, alt: '10DEAN'},
+    {image: canadaFall, alt: 'canada fall'},
+    {image: chocolateBuilding, alt: 'chocolate object'},
+    {image: cliffAndTree, alt: 'cliff and tree'},
+    {image: market, alt: 'Saint Lawrence Market'},
+    {image: library, alt: 'toronto public library'},
+    {image: niagaraBehind, alt: 'niagara behind fall'},
+    {image: nightBear, alt: 'night amusement park'},
+    {image: oldBuilding, alt: 'old building'},
+    {image: torontoUnivercity, alt: 'toronto univercity'},
+    {image: turtle, alt: 'turtle'},
+    {image: rappar, alt: 'rappar'},
+    {image: squirrel, alt: 'squirrel'},
 ];
 
 export const firstCanadaImages = canadaImages.slice(0, 4);
