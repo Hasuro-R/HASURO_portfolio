de57931023354e6b5d327b9e2c58590481ae20f1
HASURO <rentaro170928@icloud.com>
2023-09-10 01:08:30 +0900

    add: fish album

--- src/lib/image/fishImages.ts
+++ src/lib/image/fishImages.ts
@@ -18,28 +18,40 @@ import simesaba from '@/public/album/fish/simesaba.jpg';
 import siromiSushi from '@/public/album/fish/siromi_sushi.jpg';
 import sashimori from '@/public/album/fish/sashimori.jpg';
 import shiromiSashimi from '@/public/album/fish/shiromi_sashimi.jpg';
+import akayagara from '@/public/album/fish/akayagara.png';
+import buriSashiMori from '@/public/album/fish/buri_sashimi.png';
+import eikimo from '@/public/album/fish/eikimo.png';
+import hiramasaSashimi from '@/public/album/fish/hiramasa_sashimi.png';
+import madaraSashimi from '@/public/album/fish/madara_sashimi.jpg';
+import madara from '@/public/album/fish/madara.png';
 
 export const fishImages = [
     {image: sashimori, alt: 'sashimi'},
     {image: carpaccio, alt: 'carpaccio'},
     {image: chouzame, alt: 'chouzame'},
     {image: nannyouakamutu, alt: 'nannyouakamutu'},
-    {image: simesaba, alt: 'simesaba'},
     {image: buriSashimi, alt: 'buri sashimi'},
+    {image: simesaba, alt: 'simesaba'},
+    {image: buriSashiMori, alt: 'buri sashimi'},
     {image: aohata, alt: 'aohata'},
     {image: chouzameAll, alt: 'chouzame'},
     {image: chouzameSashimi, alt: 'chouzame sashimi'},
-    {image: chouzameSushi, alt: 'chouzame sushi'},
+    {image: akayagara, alt: 'akayagara'},
     {image: burisyabu, alt: 'burisyabu'},
     {image: sawaraDressing, alt: 'sawara'},
+    {image: hiramasaSashimi, alt: 'hiramasa sashimi'},
     {image: kasago, alt: 'kasago'},
     {image: kinki, alt: 'kinki'},
     {image: kinkiSashimi, alt: 'kinki sashimi'},
     {image: itoyoriSushi, alt: 'itoyori sushi'},
+    {image: eikimo, alt: 'eikimo'},
     {image: shiromiSashimi, alt: 'sashimi'},
     {image: oomonhata, alt: 'oomonhata'},
     {image: kawahagiSashimi, alt: 'kawahagi sashimi'},
+    {image: madaraSashimi, alt: 'madara sashimi'},
     {image: siromiSushi, alt: 'sushi'},
+    {image: chouzameSushi, alt: 'chouzame sushi'},
+    {image: madara, alt: 'madara'},
 ];
 
 export const firstFishImages = fishImages.slice(0, 4);
