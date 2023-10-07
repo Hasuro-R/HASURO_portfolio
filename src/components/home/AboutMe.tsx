072c0b6441ae6558f9924b5f3ab274a56152918b
HASURO <rentaro170928@icloud.com>
2023-10-07 12:37:55 +0900

    fix: change age #7

--- src/components/home/AboutMe.tsx
+++ src/components/home/AboutMe.tsx
@@ -1,8 +1,11 @@
 import Link from 'next/link';
 import ReadMoreButton from '../UI/button/ReadMoreButton';
 import styles from './home.module.scss';
+import { returnAge } from '@/lib/profile/returnAge';
 
 export default function AboutMe() {
+    const age = returnAge();
+
     return (
         <div className={styles['container']}>
             <div className={styles['title-container']}>
@@ -12,7 +15,7 @@ export default function AboutMe() {
                 </Link>
             </div>
             <div className={styles['text-container']}>
-                <span>ものづくりが大好きな17歳</span>
+                <span>ものづくりが大好きな{age}歳</span>
                 <span>主にWebアプリ開発（Next.js・Ruby on Rails）を中心に行ってきたが、最近はFlutterを用いたモバイルアプリ開発に挑戦中</span>
                 <span>アプリ開発したり、魚捌いたり、スイーツ作ったり、趣味は多岐にわたる</span>
                 <span>人生のモットーは【楽しく生きる！！】</span>
