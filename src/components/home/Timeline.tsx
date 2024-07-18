ceb84d7729b3ebc18ed20b3f8c341fc108316a1c
HASURO <rentaro170928@icloud.com>
2024-07-18 21:50:03 +0900

    add: top timeline

--- src/components/home/Timeline.tsx
+++ src/components/home/Timeline.tsx
@@ -1,10 +1,14 @@
 import Link from 'next/link';
 import ReadMoreButton from '../UI/button/ReadMoreButton';
 import styles from './home.module.scss';
-import { releasedTimelines } from '@/lib/timeline/timelines';
+import { topTimelines } from '@/lib/timeline/timelines';
 import TimelineBox from '../timeline/TimelineBox';
+import { timelineType } from '@/types/timelineType';
 
 export default function Timeline() {
+    const timeline23 = topTimelines.filter((timeline) => timeline.year === '2023');
+    const timeline24 = topTimelines.filter((timeline) => timeline.year === '2024');
+
     return (
         <div className={styles['container']}>
             <div className={styles['title-container']}>
@@ -13,24 +17,37 @@ export default function Timeline() {
                     <ReadMoreButton />
                 </Link>
             </div>
-            <div className={styles['timeline-container']}>
-                <p className={styles['timeline-year']}>2023</p>
-                <div className={styles['timeline-box-container']}>
-                {releasedTimelines.map((item, key) => {
-                    return (
-                        <TimelineBox
-                            title={item.title}
-                            date={item.date}
-                            feature={item.feature}
-                            kind={item.kind}
-                            number={key}
-                            end={releasedTimelines.length}
-                            key={key}
-                        />
-                    )
-                })}
-                </div>
-            </div>
+            <TimeLineSection year='2024' timelines={timeline24} />
+            <TimeLineSection year='2023' timelines={timeline23} />
         </div>
     )
+}
+
+function TimeLineSection({
+    year,
+    timelines,
+}: {
+    year: string,
+    timelines: timelineType[],
+}) {
+    return (
+        <div className={styles['timeline-container']}>
+        <p className={styles['timeline-year']}>{year}</p>
+        <div className={styles['timeline-box-container']}>
+        {timelines.map((item, key) => {
+            return (
+                <TimelineBox
+                    title={item.title}
+                    date={item.date}
+                    feature={item.feature}
+                    kind={item.kind}
+                    number={key}
+                    end={timelines.length}
+                    key={key}
+                />
+            )
+        })}
+        </div>
+    </div>
+    );
 }
