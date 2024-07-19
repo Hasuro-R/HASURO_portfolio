0fd42b587ab5a3041d89162a4c49cc33391e6b64
HASURO <rentaro170928@icloud.com>
2024-07-19 14:29:25 +0900

    fix: change about me #42

--- src/components/home/AboutMe.tsx
+++ src/components/home/AboutMe.tsx
@@ -16,7 +16,8 @@ export default function AboutMe() {
             </div>
             <div className={styles['text-container']}>
                 <span>ものづくりが大好きな{age}歳</span>
-                <span>主にWebアプリ開発（Next.js・Ruby on Rails）を中心に行ってきたが、最近はFlutterを用いたモバイルアプリ開発に挑戦中</span>
+                <span>Next.jsやRuby on Railsを用いたWeb開発から始め、最近はSwiftUIやGoを用いたアプリ開発にも挑戦中</span>
+                <span>現在はWebフロント・モバイル・バックエンドエンジニアの長期インターンに参加している</span>
                 <span>アプリ開発したり、魚捌いたり、スイーツ作ったり、趣味は多岐にわたる</span>
                 <span>人生のモットーは【楽しく生きる！！】</span>
             </div>
