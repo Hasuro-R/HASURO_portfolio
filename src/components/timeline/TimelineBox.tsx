6a69f6370917dda3f1dc775e5bc2c4cceebc22c6
HASURO <rentaro170928@icloud.com>
2025-03-19 17:01:45 +0900

    fix: timeline ui #50

--- src/components/timeline/TimelineBox.tsx
+++ src/components/timeline/TimelineBox.tsx
@@ -1,4 +1,5 @@
 import { returnTech } from '@/lib/work/returnTech';
+import { CgShapeCircle } from 'react-icons/cg';
 import styles from './timeline.module.scss';
 
 export default function TimelineBox({
@@ -7,40 +8,51 @@ export default function TimelineBox({
     title: string, date: string, feature?: {url: string, description: string, techs: string[]}, kind: string, number: number, end: number,
 }) {
     const isEnd = number === end - 1;
-    let icon;
-    let titleText;
+    let icon: string;
+    let titleText: JSX.Element;
     if (kind === 'released') {
         icon = '🎊';
-        titleText = <p className={styles['timeline-box-title']}>Released <a href={feature?.url} className={styles['title-url']} target='_blank'>{title}</a><span>{date}</span></p>;
+        titleText = <p>Released <a href={feature?.url} className={styles['title-url']} target='_blank'>{title}</a></p>;
     } else if (kind === 'award') {
         icon = '🏆';
-        titleText = <p className={styles['timeline-box-title']}>{title}<span>{date}</span></p>;
+        titleText = <p>{title}</p>;
     } else if (kind === 'writing') {
         icon = '✍️';
-        titleText = <p className={styles['timeline-box-title']}>Published an article on <a href={feature?.url} className={styles['title-url']} target='_blank'>{title}</a><span>{date}</span></p>;
+        titleText = <p>Published an article on <a href={feature?.url} className={styles['title-url']} target='_blank'>{title}</a></p>;
     } else if (kind === 'internship') {
         icon = '💻';
-        titleText = <p className={styles['timeline-box-title']}>{title}<span>{date}</span></p>;
+        titleText = <p>{title}</p>;
     } else if (kind === 'hackathon') {
         icon = '🔥';
-        titleText = <p className={styles['timeline-box-title']}>{title}<span>{date}</span></p>;
+        titleText = <p>{title}</p>;
     } else {
-        icon = <div className={styles['timeline-circle']}></div>;
-        titleText = <p className={styles['timeline-box-title']}>{title}<span>{date}</span></p>;
+        icon = '';
+        titleText = <p>{title}</p>;
     };
 
     return (
         <div className={styles['timeline-box']}>
             <div className={styles[isEnd ? 'timeline-right-end' : 'timeline-right']} style={{paddingBottom: isEnd ? '0' : ''}}>
-                <div className={styles['timeline-icon']}>{icon}</div>
-                    {titleText}
-                {feature && (
-                    <div className={styles['feature-box']}>
-                        {feature.description.split('\n').map((item, key) => {
-                            return (
-                                <p className={styles['feature-box-text']} key={key}>{item}</p>
-                            )
-                        })}
+                <div className={styles['timeline-icon']}>
+                    <CgShapeCircle />
+                </div>
+                <div className={styles['timeline-box-cn']}>
+                    <div className={styles['timeline-box-title-cn']}>
+                        <div className={styles['timeline-box-title-wr']}>
+                            {icon}
+                            {titleText}
+                        </div>
+                        <p className={styles['timeline-box-title-date']}>{date}</p>
+                    </div>
+                    {feature && (
+                        <div className={styles['timeline-box-feature-cn']}>
+                        <div>
+                            {feature.description.split('\n').map((item, key) => {
+                                return (
+                                    <p className={styles['feature-box-text']} key={key}>{item}</p>
+                                )
+                            })}
+                        </div>
                         {feature.techs.length !== 0 && (
                             <div className={styles['techs-container']}>
                             {feature.techs.map((item, key) => {
@@ -56,8 +68,9 @@ export default function TimelineBox({
                             })}
                             </div>
                         )}
-                    </div>
-                )}
+                        </div>
+                    )}
+                </div>
             </div>
         </div>
     )
