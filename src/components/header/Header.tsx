fc5261b4ec2574f1fdc8c521648f0b4b6bbad4a5
HASURO <rentaro170928@icloud.com>
2025-07-07 04:27:04 +0900

    fix: indent

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -5,25 +5,26 @@ import MainIcon from '../account/MainIcon';
 import styles from './header.module.scss';
 
 export default function Header() {
-    let name = 'HASURO';
-    if (typeof window !== 'undefined') {
-        const hostname = window.location.hostname;
-        if (hostname === 'inoren.me') {
-            name = 'いのれん';
-        }
-    }
+  let name = 'HASURO';
 
-    return (
-        <div className={styles['top-account-container']}>
-            <div className={styles['top-account-container-left']}>
-                <div className={styles['top-account-icon-box']}>
-                    <MainIcon />
-                </div>
-                <p className={styles['top-account-name']}>{name}</p>
-            </div>
-            <div className={styles['top-account-container-right']}>
-                <AccountBoxes />
-            </div>
+  if (typeof window !== 'undefined') {
+    const hostname = window.location.hostname;
+    if (hostname === 'inoren.me') {
+      name = 'いのれん';
+    }
+  }
+
+  return (
+    <div className={styles['top-account-container']}>
+      <div className={styles['top-account-container-left']}>
+        <div className={styles['top-account-icon-box']}>
+          <MainIcon />
         </div>
-    );
+        <p className={styles['top-account-name']}>{name}</p>
+      </div>
+      <div className={styles['top-account-container-right']}>
+        <AccountBoxes />
+      </div>
+    </div>
+  )
 }
--- src/components/header/SubHeader.tsx
+++ src/components/header/SubHeader.tsx
@@ -1,25 +1,24 @@
 'use client'
 
 import Link from 'next/link';
-import styles from './header.module.scss';
-import {IoIosArrowForward} from 'react-icons/io';
 import Image from 'next/image';
-import topIcon from '@/public/HASURO_portfolio_icon.png';
+import { IoIosArrowForward } from 'react-icons/io';
+import topIcon from '@/public/HASURO_portfolio_icon.webp';
 import ColorThemeMenu from '../UI/menu/ColorThemeMenu';
+import styles from './header.module.scss';
 
 export default function SubHeader({title}: {title: string}) {
-
-    return (
-        <div className={styles['sub-header-container']}>
-            <div className={styles['sub-header-left']}>
-                <Image src={topIcon} alt='top icon' width={50} className={styles['top-image']} />
-                <div className={styles['sub-header-left-text-container']}>
-                    <Link href='/' className={styles['top-link']}>Home</Link>
-                    <div className={styles['separate-text']}><IoIosArrowForward /></div>
-                    <p className={styles['top-link']}>{title}</p>
-                </div>
-            </div>
-            <ColorThemeMenu />
+  return (
+    <div className={styles['sub-header-container']}>
+      <div className={styles['sub-header-left']}>
+        <Image src={topIcon} alt='top icon' width={50} className={styles['top-image']} />
+        <div className={styles['sub-header-left-text-container']}>
+          <Link href='/' className={styles['top-link']}>Home</Link>
+          <div className={styles['separate-text']}><IoIosArrowForward /></div>
+          <p className={styles['top-link']}>{title}</p>
         </div>
-    )
+      </div>
+      <ColorThemeMenu />
+    </div>
+  )
 }
